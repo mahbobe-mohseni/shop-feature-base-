7ff17b2a4026bd4c2afdca7e74a18c4345a61132
mahbobe-mohseni <mahbobe.mohsenee@gmail.com>
2025-06-08 09:45:00 -0400

    edit config files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "card-gray": "red",
+      },
+    },
+  },
+  plugins: [],
+};
