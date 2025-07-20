f971c0a5e15316ab487f7592104cc6ea6bfd73a6
mahbobe-mohseni <mahbobe.mohsenee@gmail.com>
2025-07-20 04:25:26 -0400

    refactor: update utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { SignJWT } from "jose";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function createToken(data: any) {
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+  const token = await new SignJWT({
+    id: data._id.toString(),
+    phone: data.phone,
+  })
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("1d")
+    .sign(secret);
+
+  return token;
 }
